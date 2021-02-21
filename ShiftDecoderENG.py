2a15f932960ffd5d0dd23383907a65e7d17cf7d2
k0rnh0lio <63884786+k0rnh0lio@users.noreply.github.com>
2021-02-21 00:18:30 -0500

    Create ShiftDecoderENG.py

---
+++ ShiftDecoderENG.py
@@ -0,0 +1,17 @@
+# This will decode and attempt to identify english words in the decoded cipher.
+
+import enchant
+d = enchant.Dict("en_US")
+
+cipher=input("Enter Caesar Shift Cipher to Decode: ")
+for n in range(26):
+    decode=""
+    for x in range(0,len(cipher)):
+        if ord(cipher[x]) in range(97,123):
+            decode+=(chr(((ord(cipher[x])-96+n)%26)+97))
+        elif ord(cipher[x]) in range(65,91):
+            decode+=(chr(((ord(cipher[x])-64+n)%26)+65))
+        else:
+            decode+=cipher[x]
+    check = d.check(decode)
+    print((n+1),decode,check)
