232080fd92b57d69d756d9878db653405f6c6cbd
k0rnh0lio <63884786+k0rnh0lio@users.noreply.github.com>
2021-02-21 00:47:49 -0500

    Update ShiftDecoderENG.py

--- ShiftDecoderENG.py
+++ ShiftDecoderENG.py
@@ -6,12 +6,18 @@ d = enchant.Dict("en_US")
 cipher=input("Enter Caesar Shift Cipher to Decode: ")
 for n in range(26):
     decode=""
+    wordfound=""
     for x in range(0,len(cipher)):
         if ord(cipher[x]) in range(97,123):
             decode+=(chr(((ord(cipher[x])-96+n)%26)+97))
         elif ord(cipher[x]) in range(65,91):
             decode+=(chr(((ord(cipher[x])-64+n)%26)+65))
+        elif ord(cipher[x]) in range (32,33):
+            checkword=d.check(decode)
+            if checkword:
+                wordfound=("Found!")
+            decode+=cipher[x]
         else:
             decode+=cipher[x]
-    check = d.check(decode)
-    print((n+1),decode,check)
+    check=d.check(decode)
+    print((n+1),decode,wordfound)
